bcf37571736a5064bf4464e61d3006d2299b450d
Daniel Ronkainen <daniel.ronkainen@wazabii.se>
2026-08-26 22:48:26 +0200

    feat: create TableComp and SegmentOverview

--- src/components/segment/SegmentOverview.tsx
+++ src/components/segment/SegmentOverview.tsx
@@ -1,33 +1,52 @@
-import {useTranslation} from "@/i18n/useTranslation.ts";
-import {createGidTypes} from "@/segments/Segments.ts";
+import {createGidTypes, createSegments} from "@/segments/Segments.ts";
 import {useLocale} from "@/i18n/LocaleContext.tsx";
 
 type SegmentOverviewProps = {
   title: string;
 };
 
+type GidTypes = ReturnType<typeof createGidTypes>;
+type Segments = ReturnType<typeof createSegments>;
+
+type TableCompProps = {
+  data: GidTypes;
+};
+
+function TableComp({data}: TableCompProps) {
+  const {lang} = useLocale();
+  const segments = createSegments(lang);
+
+  return Object.entries(data).map(([key, row]) => (
+    <tr key={key}>
+      <td>{row.prefix}</td>
+
+      {row.layout.flatMap((value, layoutIndex) => {
+        const elemKey = (typeof value === "string" ? "seg-"+value : "seg-pad").toLowerCase();
+        const elemValue = (typeof value === "string" ? "" : "");
+        const length =
+          typeof value === "string"
+            ? segments[value as keyof Segments].length
+            : value.index.length;
+
+        return Array.from({length}, (_, index) => (
+          <td key={`${layoutIndex}-${index}`} className={elemKey}>{elemValue}</td>
+        ));
+      })}
+    </tr>
+  ));
+}
+
 function SegmentOverview({title}: SegmentOverviewProps) {
   const {lang} = useLocale();
-  const {t} = useTranslation();
   const test = createGidTypes(lang);
-
-  const TableComp = ({data}) => {
-
-    return Object.entries(data).map(([key, row]) => (
-      <tr key={key}>
-        <td>{row.prefix}</td>
-        <td>0</td>
-      </tr>
-    ));
-  };
-
 
   return (
     <>
       <h2>{title}</h2>
+
       <table>
         <tbody>
-          <TableComp data={test} />
+        <TableComp data={test} />
         </tbody>
       </table>
     </>
